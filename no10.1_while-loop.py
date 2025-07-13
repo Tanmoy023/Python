595b57d11b9ad4f8d5d3d067e944e1eba667b3e9
Tanmoy Patra <tanmoypatra369@gmail.com>
2025-07-13 17:04:47 +0530

    Add files via upload

---
+++ no10.1_while-loop.py
@@ -0,0 +1,18 @@
+# While Loop
+
+# i = 0
+# num = int(input("Enter the number : "))
+# while i < num:
+#     print(i, end=" ")
+#     i = i+1
+    
+# count = 5
+# while count > 0:
+#     print(count, end=" ")
+#     count = count - 1
+
+# While True
+i = 1
+while True:
+    print(i," time Hellow World")
+    i += 1
